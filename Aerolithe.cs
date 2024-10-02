1dae9072020355229e4a0d3d32f0a221555bb3c9
Patrick Geoffroy <patrick.geoffroy@montreal.ca>
2024-10-02 13:36:57 -0400

    Premier push pour tester

--- Aerolithe.cs
+++ Aerolithe.cs
@@ -266,7 +266,7 @@ namespace Aerolithe
         private void btn_communicationUDP_Click(object sender, EventArgs e)
         {
            
-            // C'est bien rendu
+            // Blah blah blah 
 
         }
 
