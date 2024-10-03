bab784307304cc3e744d2b39844f582229f1ff1d
Patrick Geoffroy <patrick.geoffroy@montreal.ca>
2024-10-03 19:37:41 -0400

    testing two three

--- Aerolithe.cs
+++ Aerolithe.cs
@@ -278,6 +278,7 @@ namespace Aerolithe
         {
             try
             {
+               
                 using (Ping ping = new Ping())
                 {
                     PingReply reply = ping.Send(host);
